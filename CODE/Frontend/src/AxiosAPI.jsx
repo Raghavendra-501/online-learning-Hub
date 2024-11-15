57bdff751da1a948daf5421ad8ff8fb3fbe1c6c0
Raghavendra-501 <228x5a0501@khitguntur.ac.in>
2024-11-15 20:48:49 +0530

    Delete CODE/Frontend directory

--- CODE/Frontend/src/AxiosAPI.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const url="http://localhost:5000/api/";
-
-export const npmURL="http://localhost:5000/"
-const AxiosApi=axios.create({
-    baseURL:url
-})
-export default AxiosApi;
