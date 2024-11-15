992d8a648ff7e473ff70cf1cf24d96614ea20c9d
Raghavendra-501 <228x5a0501@khitguntur.ac.in>
2024-11-15 20:48:30 +0530

    Delete CODE/Backend directory

--- CODE/Backend/src/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export default {
-  PORT: process.env.PORT || 5000,
-  DB_URL:
-    process.env.DB_URL ||
-    "mongodb://localhost:27017/Online_Learning_Platform?authSource=admin",
-};
